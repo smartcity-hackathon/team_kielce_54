8f47520585ee1d4b08ac4788cb1038728b96671b
Karol Topolski <ktopolski.it@gmail.com>
2018-06-15 23:50:35 +0200

    Importing legacy data from CSVs

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,18 @@
 class Project < ApplicationRecord
   belongs_to :category, foreign_key: :category_id, inverse_of: :projects
   belongs_to :user, foreign_key: :user_id, inverse_of: :projects
+
+  enum budget_type: {
+    small: 'small',
+    big: 'big'
+  }
+
+  enum status: {
+    submitted: 'submitted',
+    accepted: 'accepted',
+    ready_for_voting: 'ready_for_voting',
+    voted_in_category: 'voted_in_category',
+    ready_for_city_voting: 'ready_for_city_voting',
+    rejected: 'rejected'
+  }
 end
