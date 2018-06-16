0a774eb7b99eee3a9b7b3c4c0bf49483d3c596be
Karol Topolski <ktopolski.it@gmail.com>
2018-06-16 09:26:57 +0200

    /api/my/projects - index page for current user's projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :sessions, only: :create
+    namespace :my do
+      resources :projects, only: :index
+    end
 
     resources :categories, only: :index, param: :name do
       resources :projects, only: %i[index show create] do
